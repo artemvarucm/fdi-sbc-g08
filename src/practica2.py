7231de8eae325dd6b780ed0a26f8ee1486e81ace
MarioBaldocchi <marbaldo@ucm.es>
2024-10-03 15:37:16 +0200

    lectura trabajos y habilidades

--- src/practica2.py
+++ src/practica2.py
@@ -5,3 +5,13 @@ hacia atrás (backward chaining), incorporando lógica difusa
 Recibe como parametro el archivo de base de conocimiento
 '''
 
+with open("../base_laboral.txt", "r", newline="\n") as f:
+    lineas = [line.rstrip() for line in f]
+
+reglas = lineas[lineas.index('## Trabajos') + 1:lineas.index('## Habilidades')]
+habilidades = lineas[lineas.index('## Habilidades') + 1:]
+
+print(f"reglas {reglas}")
+print(f"habilidades {habilidades}")
+
+
