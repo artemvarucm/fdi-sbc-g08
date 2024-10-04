83f20a730f33213cd0bf246f3d6e0e6c2556822b
MarioBaldocchi <marbaldo@ucm.es>
2024-10-04 14:25:44 +0200

    añadido argumentos script pr2

--- src/practica2.py
+++ src/practica2.py
@@ -5,7 +5,14 @@ hacia atrás (backward chaining), incorporando lógica difusa
 Recibe como parametro el archivo de base de conocimiento
 '''
 
-with open("../base_laboral.txt", "r", newline="\n") as f:
+import argparse
+
+parser = argparse.ArgumentParser(description="Sistema basado en reglas capaz de realizar razonamiento hacia atrás (backward chaining), incorporando lógica difusa")
+# Obligatorios
+parser.add_argument('base_conocimiento', required=True, help="Ruta del archivo de la base de conocimiento")
+args = parser.parse_args()
+
+with open(args[0], "r", newline="\n") as f:
     lineas = [line.rstrip() for line in f]
 
 reglas = lineas[lineas.index('## Trabajos') + 1:lineas.index('## Habilidades')]
